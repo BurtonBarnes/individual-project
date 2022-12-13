ed7a6dc7b4520d570c1ce77de1dbf2aad96fa531
Burton Barnes <barnesburton@gmail.com>
2022-12-13 14:17:54 -0600

    model

--- model.py
+++ model.py
@@ -50,3 +50,128 @@ def model_prep(train, validate, test):
     test_y = test[['MeanTemp']].reset_index(drop=True)
     
     return train_X, validate_X, test_X, train_y, validate_y, test_y
+
+
+
+
+def get_mean(train_y, validate_y):
+    # We need y_train and y_validate to be dataframes to append the new columns with predicted values.
+    y_train = pd.DataFrame(train_y)
+    y_validate = pd.DataFrame(validate_y)
+    
+    # Predict property_value_pred_mean
+    MeanTemp_pred_mean = train_y.MeanTemp.mean()
+    train_y['MeanTemp_pred_mean'] = MeanTemp_pred_mean
+    validate_y['MeanTemp_pred_mean'] = MeanTemp_pred_mean
+    
+    # RMSE of property_value_pred_mean
+    rmse_train = mean_squared_error(y_train.MeanTemp,
+                                y_train.MeanTemp_pred_mean) ** .5
+    rmse_validate = mean_squared_error(y_validate.MeanTemp, y_validate.MeanTemp_pred_mean) ** (1/2)
+    
+    print("RMSE using Mean\nTrain/In-Sample: ", round(rmse_train, 2), 
+      "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+    
+    
+    
+def linear_regression(train_X, train_y, validate_X, validate_y):
+    lm = LinearRegression(normalize=True)
+    lm.fit(train_X, train_y.MeanTemp)
+    train_y['MeanTemp_pred_lm'] = lm.predict(train_X)
+    
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.MeanTemp, train_y.MeanTemp_pred_lm) ** (1/2)
+
+    # predict validate
+    validate_y['MeanTemp_pred_lm'] = lm.predict(validate_X)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.MeanTemp, validate_y.MeanTemp_pred_lm) ** (1/2)
+
+    print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+def lassolars(train_X, train_y, validate_X, validate_y):
+    # create the model object
+    lars = LassoLars(alpha=1)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series!
+    lars.fit(train_X, train_y.MeanTemp)
+
+    # predict train
+    train_y['MeanTemp_pred_lars'] = lars.predict(train_X)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.MeanTemp, train_y.MeanTemp_pred_lars) ** (1/2)
+
+    # predict validate
+    validate_y['MeanTemp_pred_lars'] = lars.predict(validate_X)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.MeanTemp, validate_y.MeanTemp_pred_lars) ** (1/2)
+
+    print("RMSE for Lasso + Lars\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+def polynomial(train_X, train_y, validate_X, validate_y, test_X):
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=2)
+
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(train_X)
+
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(validate_X)
+    X_test_degree2 =  pf.transform(test_X)
+    
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, train_y.MeanTemp)
+
+    # predict train
+    train_y['MeanTemp_pred_lm2'] = lm2.predict(X_train_degree2)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.MeanTemp, train_y.MeanTemp_pred_lm2) ** (1/2)
+
+    # predict validate
+    validate_y['MeanTemp_pred_lm2'] = lm2.predict(X_validate_degree2)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.MeanTemp, validate_y.MeanTemp_pred_lm2) ** 0.5
+
+    print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+
+
+
+def lassolars_test(test_X, test_y):
+    # create the model object
+    lars = LassoLars(alpha=1)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series!
+    lars.fit(test_X, test_y.MeanTemp)
+
+    # predict test
+    test_y['MeanTemp_pred_lars'] = lars.predict(test_X)
+
+    # evaluate: rmse
+    rmse_test = mean_squared_error(test_y.MeanTemp, test_y.MeanTemp_pred_lars) ** (1/2)
+
+    print("RMSE for Lasso + Lars\nTest/In-Sample: ", rmse_test)
+    return rmse_test
